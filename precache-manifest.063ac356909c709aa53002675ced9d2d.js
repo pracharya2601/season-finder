1fb7ab79065a60918555138c227f61bc85ab2a32
Prakash Acharya <pracharya2601@gmail.com>
2020-05-04 12:27:55 -0700

    Updates

---
+++ precache-manifest.063ac356909c709aa53002675ced9d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375b88a31a39e97341a7cab202218677",
+    "url": "/season-finder/index.html"
+  },
+  {
+    "revision": "c2da8a37ce7601b242c0",
+    "url": "/season-finder/static/css/main.2ba84156.chunk.css"
+  },
+  {
+    "revision": "d2de147a0ed7b689750a",
+    "url": "/season-finder/static/js/2.81ef6343.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/season-finder/static/js/2.81ef6343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2da8a37ce7601b242c0",
+    "url": "/season-finder/static/js/main.20c8b480.chunk.js"
+  },
+  {
+    "revision": "ccafa95b4259fd344836",
+    "url": "/season-finder/static/js/runtime-main.dcf10308.js"
+  }
+]);
